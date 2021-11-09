73c288f8b126bec2b3a8ae02d2ee05504fe318fa
Alicia AyersLott <amayers92@gmail.com>
2021-11-09 15:42:44 -0600

    working on profile view

---
+++ src/components/auth/Profile.js
@@ -0,0 +1,50 @@
+import userEvent from "@testing-library/user-event"
+import React, { useEffect, useState } from "react"
+import { getProfile } from "./ProfileManager.js"
+
+export const Profile = () => {
+    const [profile, changeProfile] = useState({})
+
+    useEffect(() => {
+        getProfile().then(data => changeProfile(data))
+    }, [])
+
+    return (
+        <article className="profile">
+            <header>
+                <h1>Your Profile</h1>
+            </header>
+            <section className="profile__info">
+                <header className="profile__header">
+                    <h3>Your Info</h3>
+                </header>
+                <div className="profile__name">
+                    Welcome: {profile?.gamer?.user?.first_name}
+                </div>
+                <div className="profile__username">
+                    Username: {profile?.gamer?.user?.username}
+                </div>
+                {/* TODO: show the user's bio */}
+                <div className="profile__bio">
+                    About you: {profile?.gamer?.bio}
+                </div>
+            </section>
+            <section className="profile__registrations">
+                <header className="registrations__header">
+                    <h3>Events you are attending</h3>
+                </header>
+                <div className="registrations">
+                    {/* TODO: Map through the events the user is attending */}
+                </div>
+            </section>
+            <section className="profile__registrations">
+                <header className="registrations__header">
+                    <h3>Events you are hosting</h3>
+                </header>
+                <div className="registrations">
+                    {/* TODO: Map through the events the user is hosting */}
+                </div>
+            </section>
+        </article>
+    )
+}
